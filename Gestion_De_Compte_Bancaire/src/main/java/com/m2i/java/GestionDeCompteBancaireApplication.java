4c4ae918eb9be0d4f49e2c66a4a8b9dcd92cca29
Melonil <yaci.rachidi@gmail.com>
2023-03-25 17:23:53 +0100

    go live

--- Gestion_De_Compte_Bancaire/src/main/java/com/m2i/java/GestionDeCompteBancaireApplication.java
+++ Gestion_De_Compte_Bancaire/src/main/java/com/m2i/java/GestionDeCompteBancaireApplication.java
@@ -2,9 +2,10 @@ package com.m2i.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GestionDeCompteBancaireApplication {
+public class GestionDeCompteBancaireApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionDeCompteBancaireApplication.class, args);
